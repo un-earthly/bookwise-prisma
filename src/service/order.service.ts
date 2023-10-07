69e0fc4d486e686f871f049a56f935727c5e0cea
un-earthly <vijayalamin@gmail.com>
2023-10-07 10:04:59 +0600

    order service erros resolved

--- src/service/order.service.ts
+++ src/service/order.service.ts
@@ -1,10 +1,9 @@
 import { PrismaClient } from '@prisma/client';
-import catchAsync from '../utils/catchAsync';
 
 const prisma = new PrismaClient();
 
 const OrderService = {
-    createOrder: catchAsync(async (userId: string, orderedBooks: any) => {
+    createOrder: async (userId: string, orderedBooks: any) => {
         console.log(orderedBooks.orderedBooks)
         const order = await prisma.order.create({
             data: {
@@ -14,18 +13,18 @@ const OrderService = {
             },
         });
         return order;
-    }),
+    },
 
-    getAllOrders:catchAsync( async () => {
+    getAllOrders: async () => {
         const orders = await prisma.order.findMany({
             include: {
                 orderedBooks: true,
             },
         });
         return orders;
-    }),
+    },
 
-    getSingleOrder: catchAsync(async (orderId: string) => {
+    getSingleOrder: async (orderId: string) => {
         const order = await prisma.order.findUnique({
             where: { id: orderId },
             include: {
@@ -33,7 +32,7 @@ const OrderService = {
             },
         });
         return order;
-    }),
+    },
 };
 
 export default OrderService;
